819daea57956eb805d9a7ea54bc8b25c86cb078b
Pudlich1311 <mateuszmnich13@gmail.com>
2023-06-21 20:53:26 +0200

    final?

--- pySaliencyMap.py
+++ pySaliencyMap.py
@@ -1,7 +1,5 @@
-
 import cv2
 import numpy as np
-import pySaliencyMapDefs
 
 class pySaliencyMap:
     # initialization
@@ -62,12 +60,9 @@ class pySaliencyMap:
         # max(R,G,B)
         tmp1 = cv2.max(R, G)
         RGBMax = cv2.max(B, tmp1)
-        RGBMax[RGBMax <= 0] = 0.0001    # prevent dividing by 0
-        # min(R,G)
+        RGBMax[RGBMax <= 0] = 0.0001   
         RGMin = cv2.min(R, G)
-        # RG = (R-G)/max(R,G,B)
         RG = (R - G) / RGBMax
-        # BY = (B-min(R,G)/max(R,G,B)
         BY = (B - RGMin) / RGBMax
         # clamp nagative values to 0
         RG[RG < 0] = 0
@@ -78,44 +73,32 @@ class pySaliencyMap:
         # return
         return RGFM, BYFM
     
-    ## motion feature maps
-    def Get_FM(self, src):
-        # convert scale
-        I8U = np.uint8(255 * src)
-        cv2.waitKey(10)
-        # calculating optical flows
-        if self.prev_frame is not None:
-            farne_pyr_scale= pySaliencyMapDefs.farne_pyr_scale
-            farne_levels = pySaliencyMapDefs.farne_levels
-            farne_winsize = pySaliencyMapDefs.farne_winsize
-            farne_iterations = pySaliencyMapDefs.farne_iterations
-            farne_poly_n = pySaliencyMapDefs.farne_poly_n
-            farne_poly_sigma = pySaliencyMapDefs.farne_poly_sigma
-            farne_flags = pySaliencyMapDefs.farne_flags
-            flow = cv2.calcOpticalFlowFarneback(\
-                prev = self.prev_frame, \
-                next = I8U, \
-                pyr_scale = farne_pyr_scale, \
-                levels = farne_levels, \
-                winsize = farne_winsize, \
-                iterations = farne_iterations, \
-                poly_n = farne_poly_n, \
-                poly_sigma = farne_poly_sigma, \
-                flags = farne_flags, \
-                flow = None \
-            )
-            flowx = flow[...,0]
-            flowy = flow[...,1]
-        else:
-            flowx = np.zeros(I8U.shape)
-            flowy = np.zeros(I8U.shape)
-        # create Gaussian pyramids
-        dst_x = self.Gauss_Pyr_CSD(flowx)
-        dst_y = self.Gauss_Pyr_CSD(flowy)
-        # update the current frame
-        self.prev_frame = np.uint8(I8U)
+    #orientation feature map
+    def Get_Orientation_FM(self, src):
+        # creating a Gaussian pyramid
+        GaussianI = self.Create_Gauss_Pyr(src)
+        # convoluting a Gabor filter with an intensity image to extract oriemtation features
+        GaborOutput0   = [ np.empty((1,1)), np.empty((1,1)) ]  # dummy data: any kinds of np.array()s are OK
+        GaborOutput45  = [ np.empty((1,1)), np.empty((1,1)) ]
+        GaborOutput90  = [ np.empty((1,1)), np.empty((1,1)) ]
+        GaborOutput135 = [ np.empty((1,1)), np.empty((1,1)) ]
+        for j in range(2,9):
+            GaborOutput0.append(   cv2.filter2D(GaussianI[j], -1, cv2.getGaborKernel((9, 9), 2,0, 5, 1, 0, ktype=cv2.CV_32F)) )
+            GaborOutput45.append(  cv2.filter2D(GaussianI[j], -1, cv2.getGaborKernel((9, 9),  2,45, 5, 1, 0, ktype=cv2.CV_32F))) 
+            GaborOutput90.append(  cv2.filter2D(GaussianI[j], -1, cv2.getGaborKernel((9, 9),  2,90, 5, 1, 0, ktype=cv2.CV_32F))) 
+            GaborOutput135.append( cv2.filter2D(GaussianI[j], -1, cv2.getGaborKernel((9, 9),  2,135, 5, 1, 0, ktype=cv2.CV_32F))) 
+        # calculating center-surround differences for every oriantation
+        CSD0   = self.Center_Surround_Diff(GaborOutput0)
+        CSD45  = self.Center_Surround_Diff(GaborOutput45)
+        CSD90  = self.Center_Surround_Diff(GaborOutput90)
+        CSD135 = self.Center_Surround_Diff(GaborOutput135)
+        # concatenate
+        dst = list(CSD0)
+        dst.extend(CSD45)
+        dst.extend(CSD90)
+        dst.extend(CSD135)
         # return
-        return dst_x, dst_y
+        return dst
 
     # conspicuity maps
     ## standard range normalization
@@ -130,7 +113,7 @@ class pySaliencyMap:
     ## computing an average of local maxima
     def Local_Max(self, src):
         # size
-        stepsize = pySaliencyMapDefs.default_step_local
+        stepsize = 16
         width = src.shape[1]
         height = src.shape[0]
         # find local maxima
@@ -177,10 +160,20 @@ class pySaliencyMap:
         # return
         return CCM
 
-    ## motion conspicuity map
-    def Get_Motion(self, MFM_X, MFM_Y):
-        return self.Get_Color(MFM_X, MFM_Y)
-
+    
+    ## orientation conspicuity map
+    def Get_Orientation(self, OFM):
+        OCM = np.zeros((self.height, self.width))
+        for i in range (0,4):
+            # slicing
+            nowofm = OFM[i*6:(i+1)*6]  # angle = i*45
+            # extracting a conspicuity map for every angle
+            NOFM = self.Get_Intensity(nowofm)
+            # normalize
+            NOFM2 = self.Saliency_Model_Normalization(NOFM)
+            # accumulate
+            OCM += NOFM2
+        return OCM
 
     def Get_Saliency_Map(self, src):
         # definitions
@@ -192,16 +185,13 @@ class pySaliencyMap:
         # extracting feature maps
         IFM = self.Intensity_FM(I)
         CFM_RG, CFM_BY = self.Get_Color_FM(R, G, B)
-        MFM_X, MFM_Y = self.Get_FM(I)
+        OFM = self.Get_Orientation_FM(I)
         # extracting conspicuity maps
         ICM = self.Get_Intensity(IFM)
         CCM = self.Get_Color(CFM_RG, CFM_BY)
-        MCM = self.Get_Motion(MFM_X, MFM_Y)
-        # adding all the conspicuity maps to form a saliency map
-        wi = pySaliencyMapDefs.weight_intensity
-        wc = pySaliencyMapDefs.weight_color
-        wm = pySaliencyMapDefs.weight_motion
-        SMMat = wi*ICM + wc*CCM + wm*MCM
+        OCM = self.Get_Orientation(OFM)
+
+        SMMat = ICM*0.35 + CCM*0.35 + OCM*0.3
         # normalize
         normalizedSM = self.Range_Normalization(SMMat)
         normalizedSM2 = normalizedSM.astype(np.float32)
